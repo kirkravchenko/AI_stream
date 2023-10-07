4ce0ca23227d9b8892e6fd09ec964cc07b876870
silenchio <kyrylkravchenko1994@gmail.com>
2023-10-07 19:40:53 +0200

    TTS + bot

---
+++ telegram-bot/bot.py
@@ -0,0 +1,28 @@
+import os
+import telebot
+import re
+import json
+
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+bot = telebot.TeleBot(BOT_TOKEN)
+regex = '!тема [А-Яа-я0-9 !?.,:;]+'
+topicsPath = '/Users/kirill_kravchenko/Developer/AI_stream/Assets/Scripts/Resources/topics.json'
+topicPrefix = '!тема'
+
+@bot.message_handler()
+def command_help(message):
+    list = re.split(regex, message.text)
+    if (len(list) == 2):
+        if (list[0] == '' and list[1] == ''):
+            topic = str(message.text).replace(topicPrefix, '').strip()
+            with open(topicsPath) as topics:
+                data = json.load(topics)
+            data.append(topic)
+            with open(topicsPath, 'w') as topics:
+                json.dump(data, topics, ensure_ascii=False)
+            bot.reply_to(message, 'Тема принята! 👍🏼')
+    else:
+        bot.reply_to(message, 'Формат темы некорректный')
+
+print("bot is running...")
+bot.infinity_polling()
